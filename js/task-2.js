464061643979c6c4fa71356c1ffc78c9816644a0
Michael Janzer <michaeljanzer1996@gmail.com>
2024-05-14 19:55:30 +0300

    start3

--- js/task-2.js
+++ js/task-2.js
@@ -1,50 +1,52 @@
-// function calcAverageCalories(days) {
+function calcAverageCalories(days) {
     
-//     if (days.length === 0) { return 0; }
+    if (days.length === 0) { return 0; }
 
-//     else {
+    else {
 
-//         const values = Object.values(days);
+        const values = Object.values(days);
     
-//         let totalCalories = 0;
-//         let totalDays = 0;
-//         let averageCalories = 0;
+        let totalCalories = 0;
+        let totalDays = 0;
+        let averageCalories = 0;
 
-//         for (const value of values) {
-//             totalCalories += value.calories;
-//             totalDays += 1;
-//         }
+        for (const value of values) {
+            totalCalories += value.calories;
+            totalDays += 1;
+        }
     
-//         return averageCalories = totalCalories / totalDays;
-//     }
-// }
+        return averageCalories = totalCalories / totalDays;
+    }
+}
 
 
 
-// console.log(
-//   calcAverageCalories([
-//     { day: "monday", calories: 3010 },
-//     { day: "tuesday", calories: 3200 },
-//     { day: "wednesday", calories: 3120 },
-//     { day: "thursday", calories: 2900 },
-//     { day: "friday", calories: 3450 },
-//     { day: "saturday", calories: 3280 },
-//     { day: "sunday", calories: 3300 }
-//   ])
-// ); // 3180
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
 
-// console.log(
-//   calcAverageCalories([
-//     { day: "monday", calories: 2040 },
-//     { day: "tuesday", calories: 2270 },
-//     { day: "wednesday", calories: 2420 },
-//     { day: "thursday", calories: 1900 },
-//     { day: "friday", calories: 2370 },
-//     { day: "saturday", calories: 2280 },
-//     { day: "sunday", calories: 2610 }
-//   ])
-// ); // 2270
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
 
-// console.log(
-//   calcAverageCalories([])
-// ); // 0
+console.log(
+  calcAverageCalories([])
+); // 0
+
+
